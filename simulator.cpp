a9555276e0d9e764212478f98966956a85ed99a9
ghada <gbo013@post.uit.no>
2017-02-01 11:21:03 +0100

    plane and a sphere simulation

--- simulator.cpp
+++ simulator.cpp
@@ -1,4 +1,5 @@
 #include "simulator.h"
+#include "testtorus.h"
 
 /*void SimulationController::add(DynSphere * const sphere)
 {
@@ -26,42 +27,78 @@ Simulator::Simulator(GMlib::Scene& scene) : _scene{scene}
 void Simulator::setupSimulator()
 {
 
+//    _dspheres.push_back(std::make_unique<DynSphere> ());
+//    _dspheres.back()->velocity = GMlib::Vector<double,3> (0.0f, 2.0f, 0.0f);
+
+//    _dspheres.push_back(std::make_unique<DynSphere> ());
+//    _dspheres.back()->velocity = GMlib::Vector<double,3> (0.0f, 2.0f, 3.0f);
+//    _dspheres.back()->translateGlobal(GMlib::Vector<float,3>{5.0f,3.0f,3.0f});
+
+//    _dspheres.push_back(std::make_unique<DynSphere> ());
+//    _dspheres.back()->velocity = GMlib::Vector<double,3> (0.0f, 2.0f, 3.0f);
+//    _dspheres.back()->translateGlobal(GMlib::Vector<float,3>{-5.0f,3.0f,3.0f});
+
+//    _dspheres.push_back(std::make_unique<DynSphere> ());
+//    _dspheres.back()->velocity = GMlib::Vector<double,3> (0.0f, -2.0f, 0.0f);
+//    _dspheres.back()->translateGlobal(GMlib::Vector<float,3>{0.0f,0.0f,5.0f});
+
+//   _planes.push_back(
+//      std::make_unique<Plane> (    GMlib::Point<float, 3> (-10.0f, -10.0f, 5.0f),
+//                                   GMlib::Vector<float, 3> (20.0f, 0.0f, 0.0f),
+//                                   GMlib::Vector<float, 3> (0.0f, 0.0f, -5.0f)));
+
+//    _planes.push_back(
+//      std::make_unique<Plane> (    GMlib::Point<float, 3> (10.0f, -10.0f, 5.0f),
+//                                   GMlib::Vector<float, 3> (0.0f, 20.0f, 0.0f),
+//                                   GMlib::Vector<float, 3> (10.0f, 0.0f, -5.0f)));
+
+//    _planes.push_back(
+//      std::make_unique<Plane> (    GMlib::Point<float, 3> (10.0f, 10.0f, 5.0f),
+//                                   GMlib::Vector<float, 3> (-20.0f, 0.0f, 0.0f),
+//                                   GMlib::Vector<float, 3> (0.0f, 0.0f, -5.0f)));
+
+    _planes.push_back(
+      std::make_unique<Plane> (    GMlib::Point<float, 3> (-10.0f, 5.0f, -5.0f),
+                                   GMlib::Vector<float, 3> (10.0f, -10.0f, 0.0f),
+                                   GMlib::Vector<float, 3> (10.0f, 10.0f, 0.0f)));
+    _planes.back()->setColor(GMlib::GMcolor::blue());
+
+//    _planes.push_back(std::make_unique<Plane> (    GMlib::Point<float, 3> (10.0f, 10.0f, 5.0f),
+//                                                   GMlib::Vector<float, 3> (-20.0f, -20.0f, -10.0f),
+//                                                   GMlib::Vector<float, 3> (0.0f, -10.0f, 5.0f)));
+
+//     _dspheres.push_back(std::make_unique<DynSphere> ());
+//     _dspheres.back()->velocity = GMlib::Vector<double, 3>{0.0, 2.1, 0.0};
+//     _dspheres.back()->translate(GMlib::Vector<float,3>{1.0f,-3.0f,0.0f});
+
+//     _dspheres.push_back(std::make_unique<DynSphere> ());
+//     _dspheres.back()->velocity = GMlib::Vector<double, 3>{0.0, -2.1, 0.0};;
+//     _dspheres.back()->translate(GMlib::Vector<float,3>{1.0f,3.0f,0.0f});
+
+
+//    _dspheres.push_back(std::make_unique<DynSphere> ());
+//    _dspheres.back()->velocity = GMlib::Vector<double,3> (0.0f, 0.0f, 0.0f);
+
+//    _dspheres.push_back(std::make_unique<DynSphere> ());
+//    _dspheres.back()->velocity = GMlib::Vector<double,3> (0.0f, 0.0f, 0.0f);
+//    _dspheres.back()->translateGlobal(GMlib::Vector<float,3>{5.0f,3.0f,3.0f});
+
+//    _dspheres.push_back(std::make_unique<DynSphere> ());
+//    _dspheres.back()->velocity = GMlib::Vector<double,3> (0.0f, 0.0f, 0.0f);
+//    _dspheres.back()->translateGlobal(GMlib::Vector<float,3>{-5.0f,3.0f,3.0f});
+
     _dspheres.push_back(std::make_unique<DynSphere> ());
-    _dspheres.back()->velocity = GMlib::Vector<double,3> (1.0f, 10.0f, 0.0f);
+    _dspheres.back()->velocity = GMlib::Vector<double,3> (0.0f, 0.0f, -1.0f);
+    _dspheres.back()->translateGlobal(GMlib::Vector<float,3>{0.0f,0.0f,5.0f});
 
-   /*_planes.push_back(
-      std::make_unique<Plane> (    GMlib::Point<float, 3> (-10.0f, -10.0f, 5.0f),
-                                   GMlib::Vector<float, 3> (20.0f, 0.0f, 0.0f),
-                                   GMlib::Vector<float, 3> (0.0f, 0.0f, -5.0f)));
-
-    _planes.push_back(
-      std::make_unique<Plane> (    GMlib::Point<float, 3> (10.0f, -10.0f, 5.0f),
-                                   GMlib::Vector<float, 3> (0.0f, 20.0f, 0.0f),
-                                   GMlib::Vector<float, 3> (10.0f, 0.0f, -5.0f)));
-
-    _planes.push_back(
-      std::make_unique<Plane> (    GMlib::Point<float, 3> (10.0f, 10.0f, 5.0f),
-                                   GMlib::Vector<float, 3> (-20.0f, 0.0f, 0.0f),
-                                   GMlib::Vector<float, 3> (0.0f, 0.0f, -5.0f)));
-
-    _planes.push_back(
-      std::make_unique<Plane> (    GMlib::Point<float, 3> (-10.0f, 10.0f, 5.0f),
-                                   GMlib::Vector<float, 3> (0.0f, -20.0f, 0.0f),
-                                   GMlib::Vector<float, 3> (0.0f, 0.0f, -5.0f)));*/
-
-   /* _planes.push_back(std::make_unique<Plane> (    GMlib::Point<float, 3> (10.0f, 10.0f, 5.0f),
-                                                   GMlib::Vector<float, 3> (-20.0f, -10.0f, -10.0f),
-                                                   GMlib::Vector<float, 3> (0.0f, -10.0f, -5.0f)));*/
-
-
-
-    for (auto& sphere: _dspheres)
+    for (auto& sphere: _dspheres){
         prepareAndInsert(sphere, 10, 10, 1,1);
+    }
 
     for (auto& plane: _planes)
         prepareAndInsert(plane, 10, 10, 1,1);
 
-   // auto  controller = std::make_unique<collision::MyController> ();
+     _scene.insert(&_controller);
 
-    _scene.insert(&_controller);
+
 }
